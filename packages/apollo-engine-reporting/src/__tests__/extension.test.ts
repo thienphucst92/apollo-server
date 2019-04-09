8671ba48bb6f9b75ecf1aea85290e5c8324c75c5
Jesse Rosenberger <git@jro.cc>
2019-04-09 18:34:12 +0300

    tests(a-e-r) Switch to using `describe` / `it` as with other tests.

--- packages/apollo-engine-reporting/src/__tests__/extension.test.ts
+++ packages/apollo-engine-reporting/src/__tests__/extension.test.ts
@@ -9,73 +9,75 @@ import { Request } from 'node-fetch';
 import { EngineReportingExtension } from '../extension';
 import { InMemoryLRUCache } from 'apollo-server-caching';
 
-test('trace construction', async () => {
-  const typeDefs = `
-  type User {
-    id: Int
-    name: String
-    posts(limit: Int): [Post]
-  }
-
-  type Post {
-    id: Int
-    title: String
-    views: Int
-    author: User
-  }
-
-  type Query {
-    aString: String
-    aBoolean: Boolean
-    anInt: Int
-    author(id: Int): User
-    topPosts(limit: Int): [Post]
-  }
-`;
-
-  const query = `
-    query q {
-      author(id: 5) {
-        name
-        posts(limit: 2) {
-          id
-        }
-      }
-      aBoolean
+describe('reporting extension', () => {
+  it('constructs traces', async () => {
+    const typeDefs = `
+    type User {
+      id: Int
+      name: String
+      posts(limit: Int): [Post]
     }
-`;
 
-  const schema = makeExecutableSchema({ typeDefs });
-  addMockFunctionsToSchema({ schema });
-  enableGraphQLExtensions(schema);
+    type Post {
+      id: Int
+      title: String
+      views: Int
+      author: User
+    }
 
-  const traces: Array<any> = [];
-  function addTrace(signature: string, operationName: string, trace: Trace) {
-    traces.push({ signature, operationName, trace });
-  }
+    type Query {
+      aString: String
+      aBoolean: Boolean
+      anInt: Int
+      author(id: Int): User
+      topPosts(limit: Int): [Post]
+    }
+  `;
 
-  const reportingExtension = new EngineReportingExtension({}, addTrace);
-  const stack = new GraphQLExtensionStack([reportingExtension]);
-  const requestDidEnd = stack.requestDidStart({
-    request: new Request('http://localhost:123/foo') as any,
-    queryString: query,
-    requestContext: {
-      request: {
-        query,
-        operationName: 'q',
-        extensions: {
-          clientName: 'testing suite',
+    const query = `
+      query q {
+        author(id: 5) {
+          name
+          posts(limit: 2) {
+            id
+          }
+        }
+        aBoolean
+      }
+  `;
+
+    const schema = makeExecutableSchema({ typeDefs });
+    addMockFunctionsToSchema({ schema });
+    enableGraphQLExtensions(schema);
+
+    const traces: Array<any> = [];
+    function addTrace(signature: string, operationName: string, trace: Trace) {
+      traces.push({ signature, operationName, trace });
+    }
+
+    const reportingExtension = new EngineReportingExtension({}, addTrace);
+    const stack = new GraphQLExtensionStack([reportingExtension]);
+    const requestDidEnd = stack.requestDidStart({
+      request: new Request('http://localhost:123/foo') as any,
+      queryString: query,
+      requestContext: {
+        request: {
+          query,
+          operationName: 'q',
+          extensions: {
+            clientName: 'testing suite',
+          },
         },
+        context: {},
+        cache: new InMemoryLRUCache(),
       },
-      context: {},
-      cache: new InMemoryLRUCache(),
-    },
+    });
+    await graphql({
+      schema,
+      source: query,
+      contextValue: { _extensionStack: stack },
+    });
+    requestDidEnd();
+    // XXX actually write some tests
   });
-  await graphql({
-    schema,
-    source: query,
-    contextValue: { _extensionStack: stack },
-  });
-  requestDidEnd();
-  // XXX actually write some tests
 });
